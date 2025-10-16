6db895af1ae4978e42e5215cff9e417e2c156a77
tormipoiss <158473006+tormipoiss@users.noreply.github.com>
2025-10-16 11:33:41 +0300

    Added framework files

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import http from 'http';
+
+const app = express();
+const httpServer = http.createServer(app);
+
+app.get('/', (req, res) => {
+    res.status(200).type('text/plain').send('Hello, World!');
+});
+
+const PORT = 8000;
+
+httpServer.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}/`);
+});
+
+export { httpServer, app };
