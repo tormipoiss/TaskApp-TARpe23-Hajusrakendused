31b6241db1ce584663b8a3e4f1a6f272c6f034ba
tormipoiss <158473006+tormipoiss@users.noreply.github.com>
2025-10-16 12:07:02 +0300

    changed port number

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
     res.status(200).type('text/plain').send('Hello, World!');
 });
 
-const PORT = 8000;
+const PORT = 8080;
 
 httpServer.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}/`);
