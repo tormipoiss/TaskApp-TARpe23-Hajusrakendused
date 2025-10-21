d05d7eef84a9b251834220a6925af756ad402fa1
tormipoiss <158473006+tormipoiss@users.noreply.github.com>
2025-10-21 16:32:23 +0300

    added env stuff and fixed npm install not installing for backend and frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './docs/swagger.json' with { type: 'json' };
 import  sequelize  from './data/dbConfig.js'; // test
 await sequelize.sync(); // test
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -12,10 +14,10 @@ app.get('/', (req, res) => {
     res.status(200).type('text/plain').send('Hello, World!');
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT;
 
 httpServer.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}/`);
+    console.log(`Server is running at ${process.env.SERVER_URL}:${PORT}/`);
 });
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
